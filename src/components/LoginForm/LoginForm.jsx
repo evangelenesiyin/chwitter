f0b83b1502923ec0f14f234057c5a9596d0364c1
Evangelene <siyinyap@gmail.com>
2023-10-24 11:31:41 +0800

    styles: amend style for login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,17 @@
 export default function LoginForm() {
     return (
-        <div className="col-span-1 p-4 mt-48 -ml-16">
-            <form className="bg-darkred shadow-md rounded px-8 py-8 h-96 mx-8">
+        <div className="col-span-1 mt-28 -ml-16">
+            <form className="bg-darkred shadow-md rounded px-8 py-8 mx-8">
                 <div className="pt-6">
-                    <label htmlFor="username" className="text-white text-lg mx-6">Username</label>
+                    <label htmlFor="username" className="text-white text-lg mx-8">Username</label>
                 </div>
-                    <input type="text" className="border border-white w-96 py-1 px-2 mt-1 mx-6 rounded"></input>
+                    <input type="text" className="border border-white w-3/4 py-1 px-2 mt-1 mx-8 rounded"></input>
                 <div className="pt-6">
-                    <label htmlFor="password" className="text-white text-lg mx-6">Password</label>
+                    <label htmlFor="password" className="text-white text-lg mx-8">Password</label>
                 </div>
-                    <input type="text" className="border border-white w-96 py-1 px-2 mt-1 mx-6 rounded"></input>
+                    <input type="text" className="border border-white w-3/4 py-1 px-2 mt-1 mx-8 rounded"></input>
                 <div className="mt-2">
-                    <button className="border border-coral bg-coral text-white text-lg rounded py-2 px-28 mx-20 my-16">Login</button>
+                    <button className="border border-coral bg-coral text-white text-lg rounded w-2/3 py-2 mx-16 my-8">Login</button>
                 </div>
             </form>
         </div>
