14f3eef75e9892a6f2c02a2b250f1c8358c5399f
Evangelene <siyinyap@gmail.com>
2023-10-24 11:33:44 +0800

    styles: amend style for landing page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,19 +1,25 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import NavBar from "../../components/NavBar/NavBar";
 
 function App() {
 
   return (
-    <main className="min-h-screen min-w-screen bg-beige p-0 m-0 grid grid-cols-3 gap-4">
-      <div className="col-span-1 p-4">
-        <div className="text-8xl text-white text-right font-extrabold mt-52 mb-4">EXPRESS<br /> YOURSELF<br /> WITH<br /><span className="text-black">CHWITTER</span></div>
-      </div>
-        <div className="col-span-1 mt-4">
-        <img src="./assets/landingphoto.jpeg" alt="Image" className="-ml-8" />
+    <>
+      <NavBar />
+    <main className="min-h-screen min-w-screen bg-beige p-0 m-0 grid grid-cols-3">
+      <div className="col-span-1 items-center justify-center h-screen flex">
+        <div className="text-title text-white text-right font-extrabold  break-words leading-tight mb-16">
+        <span>EXPRESS</span> <span>YOURSELF</span> <span>WITH</span> <span className="text-black">CHWITTER</span>
         </div>
-      <LoginForm />
+      </div>
+        <div className="col-span-1 items-center justify-center h-screen flex">
+        <img src="./assets/landingphoto.png" alt="Image" className="mx-auto mt-22 w-3/4" />
+        </div>
+      {/* <LoginForm /> */}
       <SignUpForm />
     </main>
+    </>
   )
 }
 
