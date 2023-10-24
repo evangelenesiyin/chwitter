654f48333df1672d855a373995d7566fda6376ed
Evangelene <siyinyap@gmail.com>
2023-10-24 19:38:08 +0800

    feat: setup users-api file

---
+++ src/utilities/users-api.js
@@ -0,0 +1,15 @@
+import sendRequest from "./send-request";
+
+const BASE_URL = "/api/users";
+
+export function signUpAPI(userData) {
+  return sendRequest(BASE_URL, "POST", userData);
+}
+
+export function loginAPI(credentials) {
+  return sendRequest(`${BASE_URL}/login`, "POST", credentials);
+}
+
+// export function checkTokenAPI() {
+//   return sendRequest(`${BASE_URL}/check-token`);
+// }
