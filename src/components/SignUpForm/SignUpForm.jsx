e318e14fe8bfa9ca40a691b42eb4d45ab97911cb
Evangelene <siyinyap@gmail.com>
2023-10-24 15:19:10 +0800

    styles: amend styles for sign up form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,7 @@
 export default function SignUpForm() {
     return(
-        <div className="col-span-1 mt-28 -ml-16">
-            <form className="bg-darkred shadow-md rounded px-8 py-8 mx-8">
+        <div className="col-span-1 -ml-16">
+            <form className="bg-darkred shadow-xl rounded-b-lg rounded-tr-lg px-8 py-8 mx-8">
                 <div className="pt-6">
                     <label htmlFor="username" className="text-white text-lg mx-8">Username</label>
                 </div>
@@ -19,7 +19,7 @@ export default function SignUpForm() {
                 </div>
                     <input type="text" className="border border-white w-4/5 py-1 px-2 mt-1 mx-8 rounded"></input>
                 <div className="mt-2">
-                    <button className="border border-coral bg-coral text-white text-lg rounded w-2/3 py-2 mx-16 my-8">Sign Up</button>
+                    <button className="border border-coral bg-coral text-white text-lg font-bold shadow-md rounded w-2/3 py-2 mx-16 my-8">Sign Up</button>
                 </div>
             </form>
         </div>
