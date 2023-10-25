67550ec6e8c464b45a5f0a0e602ac7f9f44ee0fa
Evangelene <siyinyap@gmail.com>
2023-10-25 11:46:36 +0800

    feat: add validation for login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginService } from "../../utilities/users-service";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function LoginForm({ setUser }) {
+export default function LoginForm({ setUser, status, setStatus }) {
     const [credentials, setCredentials] = useState({
     username: "",
     password: "",
@@ -21,24 +23,35 @@ export default function LoginForm({ setUser }) {
     try {
         const user = await loginService(credentials);
         if (user !== null && user !== undefined) {
-            alert(`Welcome back ${user.username}!`);
+            toast.success(`Welcome back, ${user.username}!`);
             setUser(user);
             navigate("/home");
         }
     } catch (err) {
         if (err.message === "Unexpected end of JSON input") {
-            alert("Please try again later.")
+            toast.error("Please try again.")
         } else {
-            alert("Try again.");
             setCredentials({
                 username: "",
                 password: "",
             })
         }
+        setStatus("error");
+        toast.error("Unsuccessful login attempt.")
+    } finally {
+        setStatus(null);
     }
   }
 
+  const isFormValid = () => {
+    return credentials.username && credentials.password && status !== "error";
+  };
+
+  
+
     return (
+        <>
+        <ToastContainer />
         <div className="col-span-1 -ml-16">
             <form className="bg-darkred shadow-xl rounded-b-lg rounded-tr-lg px-8 py-8 mx-8" onSubmit={handleSubmit}>
                 <div className="pt-6">
@@ -52,7 +65,8 @@ export default function LoginForm({ setUser }) {
                     onChange={handleChange}
                     autoComplete="off"
                     required
-                    className="border border-white w-4/5 py-1 px-2 mt-1 mx-8 rounded"></input>
+                    className="border border-white w-4/5 py-1 px-2 mt-1 mx-8 rounded"
+                    ></input>
                 <div className="pt-6">
                     <label htmlFor="password" className="text-white text-lg mx-8">Password</label>
                 </div>
@@ -64,13 +78,21 @@ export default function LoginForm({ setUser }) {
                     autoComplete="off"
                     onChange={handleChange}
                     required
-                    className="border border-white w-4/5 py-1 px-2 mt-1 mx-8 rounded"></input>
+                    className="border border-white w-4/5 py-1 px-2 mt-1 mx-8 rounded"
+                    ></input>
                 <div className="mt-2">
                     <button
                     type="submit"
-                    className="border border-coral bg-coral text-white text-lg font-bold shadow-md rounded w-2/3 py-2 mx-16 my-8">Login</button>
+                    className="border border-coral bg-coral text-white text-lg font-bold shadow-md rounded w-2/3 py-2 mx-16 my-8"
+                    style={{
+                    background: isFormValid() ? '' : 'darkgrey',
+                    border: isFormValid() ? '' : '2px solid darkgrey',
+                    cursor: isFormValid() ? '' : "not-allowed",
+                    }}
+                    >Login</button>
                 </div>
             </form>
         </div>
+        </>
     )
 }
