4b8c891a3d6a017a0c03172407d363ed4fd20ee4
Evangelene <siyinyap@gmail.com>
2023-10-26 11:04:04 +0800

    styles: amend border radius for side panel

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -31,8 +31,8 @@ export default function SidePanel({ setUser }) {
     return (
         <div className="relative">
             <div className="grid grid-rows-2 gap-4 h-full ml-64 mt-6">
-                <div className="bg-white w-5/6 rounded-lg">
-                    <img src="./assets/placeholderimg.jpeg" alt="header-image" className="h-44 w-full object-cover rounded-t-lg" />
+                <div className="bg-white w-5/6 rounded-sm">
+                    <img src="./assets/placeholderimg.jpeg" alt="header-image" className="h-44 w-full object-cover rounded-t-sm" />
                     <img src="./assets/placeholder2.jpeg" alt="profile-image" className="w-1/3 rounded-full mx-auto -mt-12" />
                     <div className="text-center px-4 py-2">
                         <p className="font-bold text-xl">catlover123</p>
@@ -44,7 +44,7 @@ export default function SidePanel({ setUser }) {
                 <div className="w-5/6 h-12">
                     <Collapse accordion>
                         {items.map((item) => (
-                            <Panel key={item.key} header={item.header} className="border border-white bg-white rounded-lg text-base font-bold">
+                            <Panel key={item.key} header={item.header} className="border border-white bg-white rounded-sm text-base font-bold">
                                 {item.content}
                             </Panel>
                         ))}
