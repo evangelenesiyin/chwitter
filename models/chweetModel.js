e10ae21b3d60e1a5866d3f78311aa46530ca949f
Evangelene <siyinyap@gmail.com>
2023-10-26 11:03:13 +0800

    feat: setup chweet model

---
+++ models/chweetModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const chweetSchema = new Schema(
+  {
+    postText: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 250,
+      message: "Post must be less than 250 characters",
+    },
+    postPicture: {
+      type: String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
+
+chweetSchema.virtual("s3ObjecID").get(function () {
+  const deconstructedURL = this.imageURL.split("/");
+  const s3ObjectID = deconstructedURL[deconstructedURL.length - 1];
+  return s3ObjectID;
+});
+
+module.exports = model("Chweet", chweetSchema);
