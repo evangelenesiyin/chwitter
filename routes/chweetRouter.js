663eed0cdf813b1d7b31edf0fc1f09bc9797ced0
Evangelene <siyinyap@gmail.com>
2023-10-27 03:11:54 +0800

    fix: amend router url for create post and upload image

--- routes/chweetRouter.js
+++ routes/chweetRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const chweetCtrl = require("../controllers/chweetCtrl");
 const { uploadToS3 } = require("../config/s3Middlewares");
 
-router.post("/", chweetCtrl.createPost);
-router.post("/", uploadToS3, chweetCtrl.uploadImg);
+router.post("/new/upload", uploadToS3, chweetCtrl.uploadImg);
+router.post("/new", chweetCtrl.createPost);
 router.get("/", chweetCtrl.getAllPosts);
 
 module.exports = router;
