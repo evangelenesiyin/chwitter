61e8ddf34633933f7c2fcf008dc4692cc486a39d
Evangelene <siyinyap@gmail.com>
2023-10-29 00:12:00 +0800

    feat: setup profile api utility file

---
+++ src/utilities/profile-api.js
@@ -0,0 +1,11 @@
+import sendRequest from "./send-request";
+
+const BASE_URL = "/api/profile";
+
+export function uploadToS3API(imgFormData) {
+  return sendRequest(`${BASE_URL}/new/upload`, "POST", imgFormData, true);
+}
+
+export function addProfileAPI(profileData) {
+  return sendRequest(`${BASE_URL}/new`, "POST", profileData);
+}
