2e6d39e328f97fb5f74aff84d4034f43c71f5b83
Evangelene <siyinyap@gmail.com>
2023-10-28 18:11:33 +0800

    docs: edit chweetModel to replace field names

--- models/chweetModel.js
+++ models/chweetModel.js
@@ -3,15 +3,38 @@ const { Schema, model } = mongoose;
 
 const chweetSchema = new Schema(
   {
-    postText: {
+    type: {
       type: String,
       required: true,
-      trim: true,
-      maxLength: 250,
-      message: "Post must be less than 250 characters",
+      enum: {
+        values: ["Cat", "Dog", "Rabbit", "Bird", "Hamster", "Others"],
+      },
+    },
+    breed: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female", "Not sure"],
+    },
+    sterilised: {
+      type: String,
+      required: true,
+      enum: ["Yes", "No", "Not sure"],
+    },
+    contactDetails: {
+      type: String,
+      required: true,
+    },
+    remarks: {
+      type: String,
+      maxLength: 100,
     },
     imageURL: {
       type: String,
+      required: true,
     },
     user: {
       type: Schema.Types.ObjectId,
