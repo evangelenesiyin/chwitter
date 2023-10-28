94e9ad4300dec02d8c310fd0957d9c35527aa8ba
Evangelene <siyinyap@gmail.com>
2023-10-29 00:09:03 +0800

    feat: setup profile router

---
+++ routes/profileRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const profileCtrl = require("../controllers/profileCtrl");
+const { uploadToS3 } = require("../config/s3Middlewares");
+
+router.post("/new/upload", uploadToS3, profileCtrl.uploadImg);
+router.post("/new", profileCtrl.createProfile);
+
+module.exports = router;
