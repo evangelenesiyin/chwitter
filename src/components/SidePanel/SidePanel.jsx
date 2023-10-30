8cd277a6f093cd4dcb5623fe19724be0fd0e3099
Evangelene <siyinyap@gmail.com>
2023-10-30 14:51:40 +0800

    fix: fetch profile info of user successfully

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -4,7 +4,7 @@ import { logOutService } from "../../utilities/users-service";
 
 const { Panel } = Collapse;
 
-export default function SidePanel({ setUser }) {
+export default function SidePanel({ setUser, profileInfo }) {
     const navigate = useNavigate();
     
     const handleLogOut = (e) => {
@@ -30,18 +30,18 @@ export default function SidePanel({ setUser }) {
 
     return (
         <div className="relative">
-            <div className="grid grid-rows-2 gap-4 h-auto ml-64 mt-6">
-                <div className="bg-white w-5/6 rounded-sm">
-                    <img src="./assets/placeholderimg.jpeg" alt="header-image" className="h-44 w-full object-cover rounded-t-sm" />
-                    <img src="./assets/placeholder2.jpeg" alt="profile-image" className="w-1/3 rounded-full mx-auto -mt-12" />
+            <div className="grid grid-rows-2 gap-4 h-auto w-full mt-6">
+                <div className="bg-white w-1/2 rounded-sm ml-80">
+                    <div className="h-20 bg-coral"></div>
+                    <img src={profileInfo.profilePicture} alt="profile-image" className="w-1/3 rounded-full mx-auto -mt-12" />
                     <div className="text-center px-4 py-2">
-                        <p className="font-bold text-xl">catlover123</p>
-                        <p className="text-gray-400 mb-2">@user123</p>
-                        <p className="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dolor velit, maximus vitae consectetur eu, viverra eu ipsum.</p>
-                        <p className="my-4 font-bold text-base">View full profile</p>
+                        <p className="font-bold text-xl">{profileInfo.displayName}</p>
+                        <p className="text-gray-400 mb-2">{"@"}{profileInfo.username}</p>
+                        <p className="text-gray-400">{profileInfo.bio}</p>
+                        <p className="mt-6 mb-2 font-bold text-base text-coral">View full profile</p>
                     </div>
                 </div>
-                <div className="w-5/6 h-12">
+                <div className="w-1/2 h-12 ml-80">
                     <Collapse accordion>
                         {items.map((item) => (
                             <Panel key={item.key} header={item.header} className="border border-white bg-white rounded-sm text-base font-bold">
