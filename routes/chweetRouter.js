6083ab11be6ceaf43020426bd7657c81c500faa0
Evangelene <siyinyap@gmail.com>
2023-10-30 17:06:15 +0800

    feat: add delete from S3 and delete route

--- routes/chweetRouter.js
+++ routes/chweetRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const chweetCtrl = require("../controllers/chweetCtrl");
-const { uploadToS3 } = require("../config/s3Middlewares");
+const { uploadToS3, deleteFromS3 } = require("../config/s3Middlewares");
 
 router.post("/new/upload", uploadToS3, chweetCtrl.uploadImg);
 router.post("/new", chweetCtrl.createPost);
 router.get("/", chweetCtrl.getAllPosts);
+router.delete("/:postID", deleteFromS3, chweetCtrl.del);
 
 module.exports = router;
