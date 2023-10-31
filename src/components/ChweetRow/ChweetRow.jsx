144fed7e06695c331d25c3752575db35d9b1de54
Evangelene <siyinyap@gmail.com>
2023-10-31 17:58:36 +0800

    feat: add edit chweet functionality, modal, framer-motion

--- src/components/ChweetRow/ChweetRow.jsx
+++ src/components/ChweetRow/ChweetRow.jsx
@@ -1,23 +1,58 @@
 import debug from "debug";
+import { useState } from "react";
 import { deletePostService } from "../../utilities/chweet-service";
+import EditChweet from "../EditChweet/EditChweet";
+import { formatDistanceToNow } from 'date-fns';
+import { parseISO } from 'date-fns';
 import { PiDotsThreeBold } from "react-icons/pi";
-import { AiOutlineHeart, AiOutlineRetweet } from "react-icons/ai";
-import { Dropdown, Space } from 'antd';
+import { Dropdown, Space, Modal } from 'antd';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { motion } from "framer-motion";
 
 const log = debug("chwitter:src:components:ChweetRow");
 
-export default function ChweetRow({ post, profileInfo, fetchPostData }) {
+export default function ChweetRow({ post, setPost, fetchAllPosts }) {
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const modalWidth = "40%";
+
+  const showModal = () => {
+    setOpen(true);
+  }
+
+  const handleCancel = () => {
+    setOpen(false);
+  }
 
   const items = [
   {
     label: (
-      <a target="_blank" rel="noopener noreferrer" href="/">
-        Edit Text
+      <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="/"
+      >
+        Save Post
       </a>
     ),
     key: '0',
+  },
+    {
+    label: (
+      <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="/"
+      onClick={(e) => {
+        e.preventDefault();
+        showModal();
+      }}
+      >
+        Edit Text
+      </a>
+    ),
+    key: '1',
   },
   {
     label: (
@@ -33,7 +68,7 @@ export default function ChweetRow({ post, profileInfo, fetchPostData }) {
         Delete
       </a>
     ),
-    key: '1',
+    key: '2',
   },
 ];
 
@@ -41,7 +76,7 @@ const handleDelete = async (postID) => {
     log("Deleting post with ID:", postID);
       try {
         await deletePostService(postID);
-        await fetchPostData();
+        await fetchAllPosts();
          toast.success("Deleted Chweet successfully.");
       } catch (err) {
           log("Error deleting post:", err);
@@ -52,6 +87,15 @@ const handleDelete = async (postID) => {
     return (
         <>
         <ToastContainer />
+        <Modal title="Edit your Chweet" open={open} footer={null} confirmLoading={confirmLoading} onCancel={handleCancel} width={modalWidth}>
+          <EditChweet post={post} setOpen={setOpen} setConfirmLoading={setConfirmLoading} fetchAllPosts={fetchAllPosts} />
+        </Modal>
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.6 }}
+        >
         <div className="relative">
             <div
             className="grid grid-cols-2 grid-rows-3 gap-0 border border-gray-100 bg-white h-auto w-3/4 rounded-sm"
@@ -61,15 +105,15 @@ const handleDelete = async (postID) => {
             }} >
                 <div className="bg-white col-span-1 row-span-3 h-auto">
                     <img
-                    src={profileInfo.profilePicture}
+                    src={post?.user?.profile?.profilePicture}
                     className="rounded-full h-16 mx-auto mt-2"
                     />
                 </div>
                 <div className="flex bg-white col-span-1 row-span-1 w-full my-2">
-                    <span className="font-bold">{profileInfo.displayName}</span>&nbsp;
-                    <span className="text-gray-400">{"@"}{profileInfo.username}</span>&nbsp;
+                    <span className="font-bold">{post?.user?.profile?.displayName}</span>&nbsp;
+                    <span className="text-gray-400">{"@"}{post?.user?.profile?.username}</span>&nbsp;
                     <span className="text-gray-400">·</span>&nbsp;
-                    <span className="text-gray-400">1h</span>
+                    <span className="text-gray-400">{formatDistanceToNow(parseISO(post?.createdAt))} ago</span>
                     <span className="ml-auto pr-4 text-gray-500 cursor-pointer">
                     <Dropdown
                     menu={{
@@ -86,27 +130,20 @@ const handleDelete = async (postID) => {
                 </div>
                 <div className="bg-white col-span-1 row-span-1 w-full pb-1 mt-1">
                     <div className="my-2 font-light text-sm">
-                    <div><span className="font-medium">Type:</span>{' '}{post.type}</div>
-                    <div><span className="font-semibold">Breed:</span>{' '}{post.breed}</div>
-                    <div><span className="font-semibold">Gender:</span>{' '}{post.gender}</div>
-                    <div><span className="font-semibold">Sterilised:</span>{' '}{post.sterilised}</div>
-                    <div><span className="font-semibold">Contact:</span>{' '}{post.contactDetails}</div>
-                    <div><span className="font-semibold">Remarks:</span>{' '}{post.remarks}</div>
+                    <div><span className="font-medium">Type:</span>{' '}{post?.type}</div>
+                    <div><span className="font-semibold">Breed:</span>{' '}{post?.breed}</div>
+                    <div><span className="font-semibold">Gender:</span>{' '}{post?.gender}</div>
+                    <div><span className="font-semibold">Sterilised:</span>{' '}{post?.sterilised}</div>
+                    <div><span className="font-semibold">Contact:</span>{' '}{post?.contactDetails}</div>
+                    <div><span className="font-semibold">Remarks:</span>{' '}{post?.remarks}</div>
                     </div>
                     <div className="mb-2">
-                    <img src={post.imageURL} className="h-full w-3/5" />
+                    <img src={post?.imageURL} className="h-full w-3/5" />
                     </div>
-                </div>
-                <div className="flex bg-white col-span-1 row-span-1 h-auto w-full items-center">
-                    <span className="w-6 h-6 text-gray-400 cursor-pointer">
-                        <AiOutlineHeart />
-                    </span>
-                    <span className="w-6 h-6 text-gray-400 ml-8 cursor-pointer">
-                        <AiOutlineRetweet />
-                    </span>
                 </div>
             </div>
         </div>
+        </motion.div>
     </>
     )
 }
