cd4492005e1d0328d5b61a3db052adfbe8dab2ce
Evangelene <siyinyap@gmail.com>
2023-11-01 03:08:58 +0800

    styles: adjust logo height

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
         <div className="bg-beige">
-            <Link to="/"><img src="./assets/chwitterlogo.png" alt="Chwitter" className="h-10 pl-2 pt-2" /></Link>
+            <Link to="/"><img src="./assets/chwitterlogo.png" alt="Chwitter" className="h-14 pl-2 pt-2" /></Link>
         </div>
     )
 }
