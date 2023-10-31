49be415f5ff2d6e5756482ff884850e469244072
Evangelene <siyinyap@gmail.com>
2023-10-31 17:54:51 +0800

    feat: add patch router for updateOne

--- routes/chweetRouter.js
+++ routes/chweetRouter.js
@@ -7,5 +7,6 @@ router.post("/new/upload", uploadToS3, chweetCtrl.uploadImg);
 router.post("/new", chweetCtrl.createPost);
 router.get("/", chweetCtrl.getAllPosts);
 router.delete("/:postID", deleteFromS3, chweetCtrl.del);
+router.patch("/:postID/edit", chweetCtrl.updateOne);
 
 module.exports = router;
