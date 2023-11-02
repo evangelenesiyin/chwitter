c0e348ff762472dbc1cb0ac51ec1c08c0c933731
Evangelene <siyinyap@gmail.com>
2023-11-02 09:55:22 +0800

    update side panel

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -30,15 +30,15 @@ export default function SidePanel({ setUser, profileInfo }) {
 
     return (
         <div className="relative">
-            <div className="grid grid-rows-2 gap-4 h-auto w-full mt-6">
+            <div className="grid grid-rows-2 gap-4 h-auto w-full">
                 <div className="bg-white w-1/2 rounded-sm ml-64 sm:ml-64 md:ml-64 lg:ml-64 xl:ml-64 2xl:ml-80">
                     <div className="h-20 bg-coral"></div>
-                    <img src={profileInfo.profilePicture} alt="profile-image" className="w-28 h-28 rounded-full mx-auto -mt-12 object-cover" />
+                    <img src={profileInfo?.profilePicture || "./assets/grey.png"} alt="profile-image" className="w-28 h-28 rounded-full mx-auto -mt-12 object-cover" />
                     <div className="text-center px-4 py-2">
-                        <p className="font-bold text-xl">{profileInfo.displayName}</p>
-                        <p className="text-gray-400 mb-2">{"@"}{profileInfo.username}</p>
-                        <p className="text-gray-400">{profileInfo.bio}</p>
-                        <p className="mt-6 mb-2 font-bold text-base text-coral">View full profile</p>
+                        <p className="font-bold text-xl">{profileInfo?.displayName}</p>
+                        <p className="text-gray-400 mb-2">{"@"}{profileInfo?.username}</p>
+                        <p className="text-gray-400">{profileInfo?.bio}</p>
+                        <Link to={`/${profileInfo?.username}`}><p className="mt-6 mb-2 font-bold text-base text-coral">View full profile</p></Link>
                     </div>
                 </div>
                 <div className="w-1/2 h-12 ml-64 sm:ml-64 md:ml-64 lg:ml-64 xl:ml-64 2xl:ml-80">
