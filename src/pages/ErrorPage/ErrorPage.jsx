7276abd8da9ff1eb7571b77f4ae2ef2d8f8d53d9
Evangelene <siyinyap@gmail.com>
2023-11-02 23:22:17 +0800

    feat: add Error Page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,17 @@
+import { TfiDirectionAlt } from "react-icons/tfi";
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
     return (
-        <h1>Error Page</h1>
+        <div className="min-h-screen min-w-screen bg-beige">
+            <div className="flex justify-center items-center h-screen text-center text-5xl font-bold text-darkred">
+                <div className="mb-40">
+                <TfiDirectionAlt className="w-12 h-12 mx-auto"/><br />
+                Uh oh... Page not found. <br />
+                Are you lost?<br /><br />
+                <div className="flex text-2xl justify-center items-center"><Link to="/home" className="border border-coral bg-coral py-2 px-4 w-auto text-white rounded-lg">Return to Home</Link></div>
+                </div>
+            </div>
+            </div>
     )
 }
