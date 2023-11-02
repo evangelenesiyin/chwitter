f7b10a7f7528bd6158b887a73c17b24ff57f2d56
Evangelene <siyinyap@gmail.com>
2023-11-02 23:34:55 +0800

    chore: clean up code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function NavBar() {
     return (
         <div className="bg-beige">
-            <Link to="/"><img src="./assets/chwitterlogo.png" alt="Chwitter" className="h-14 pl-2 pt-2" /></Link>
+            <img src="./assets/chwitterlogo.png" alt="Chwitter" className="h-14 pl-2 pt-2" />
         </div>
     )
 }
