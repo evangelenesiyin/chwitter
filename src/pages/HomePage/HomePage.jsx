ca3709abd8e233776769f033a99e5667a5651d2a
Evangelene <siyinyap@gmail.com>
2023-11-02 09:56:51 +0800

    feat: fix side panel and allow scrolling for form and row only

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,18 @@ import SidePanel from "../../components/SidePanel/SidePanel";
 import ChweetForm from "../../components/ChweetForm/ChweetForm";
 import ChweetRow from "../../components/ChweetRow/ChweetRow";
 
-export default function HomePage({ setUser, allPosts, setAllPosts, profileInfo, fetchAllPosts }) {
+export default function HomePage({ user, setUser, allPosts, setAllPosts, profileInfo, fetchAllPosts }) {
 
     return (
         <>
         <div className="min-h-screen min-w-screen bg-beige p-0 m-0 grid" style={{ gridTemplateColumns: "40% 60%" }}>
-        <SidePanel setUser={setUser} profileInfo={profileInfo} />
-        <div>
-        <ChweetForm allPosts={allPosts} setAllPosts={setAllPosts} profileInfo={profileInfo} fetchAllPosts={fetchAllPosts} />
-        {allPosts.map((post) => (<ChweetRow key={post._id} post={post} fetchAllPosts={fetchAllPosts} />))}
-        </div>
+            <SidePanel setUser={setUser} profileInfo={profileInfo} />
+            <div style={{ overflowY: "scroll", maxHeight: "100vh" }}>
+                <div style={{ overflow: "hidden" }}>
+                <ChweetForm allPosts={allPosts} setAllPosts={setAllPosts} profileInfo={profileInfo} fetchAllPosts={fetchAllPosts} />
+                {allPosts.map((post) => (<ChweetRow key={post._id} user={user} post={post} fetchAllPosts={fetchAllPosts} />))}
+                </div>
+            </div>
         </div>
         </>
     )
